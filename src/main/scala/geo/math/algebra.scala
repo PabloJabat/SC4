c45205aeb89329570e47c352fec175cd346b283b
Pablo Jabat <pablojabat@gmail.com>
2018-02-16 15:28:56 +0100

    Added new map matching technique

--- src/main/scala/geo/math/algebra.scala
+++ src/main/scala/geo/math/algebra.scala
@@ -6,34 +6,14 @@ import scala.math._
 
 object algebra {
 
-  def area(a: Point, b: Point, c: Point): Double = {
-    abs(a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) / 2
-  }
-
-  def height(p: Point, segment: Segment): Double = {
-    area(segment.a, segment.b, p) * 2 / segment.norm
-  }
-
-  def outOfSegment(p: Point, s: Segment): Boolean = {
-
-    val a = s.norm
-    val b = p.distToPoint(s.b)
-    val c = p.distToPoint(s.a)
-
-    val beta = acos((pow(a,2)+pow(c,2)-pow(b,2))/(2*a*c))
-    val gamma = acos((pow(a,2)+pow(b,2)-pow(c,2))/(2*a*b))
-
-    if (beta > Pi/2 || gamma > Pi/2) true else false
-  }
-
   def haversineFormula (a: Point, b: Point): Double = {
 
     val R = 6371e3
 
-    val sigma1 = a.x.toRadians
-    val sigma2 = b.x.toRadians
-    val sigmaDelta = (b.x-a.x).toRadians
-    val lambdaDelta = (b.y-a.y).toRadians
+    val sigma1 = a.lat.toRadians
+    val sigma2 = b.lat.toRadians
+    val sigmaDelta = (b.lat-a.lat).toRadians
+    val lambdaDelta = (b.lon-a.lon).toRadians
 
     val A = sin(sigmaDelta/2)*sin(sigmaDelta/2) + cos(sigma1)*cos(sigma2)*sin(lambdaDelta/2)*sin(lambdaDelta/2)
     val C = 2*atan2(sqrt(A), sqrt(1-A))
@@ -41,4 +21,14 @@ object algebra {
     R * C
 
   }
+
+  def naiveBayesClassifier (p: Point, s: Segment, stdev_b: Double = 5.0, stdev_deltaPhi: Double = 1000000.0): Double = {
+
+    val b = p.distToSegment(s)
+    val deltaPhi = p.orientationDifference(s)
+
+    pow(b,2)/stdev_b + pow(deltaPhi,2)/stdev_deltaPhi
+
+  }
+
 }
