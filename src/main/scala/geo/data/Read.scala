9efd309324d9bf72d1b140f1a8c75dd82e4ff817
Pablo Jabat <pablojabat@gmail.com>
2018-03-13 10:58:11 +0100

    added new functions for data extraction for AppPerformance

--- src/main/scala/geo/data/Read.scala
+++ src/main/scala/geo/data/Read.scala
@@ -33,7 +33,27 @@ object Read {
 
     spark.sparkContext.textFile(gpsDataPath)
       .map(line => pattern.findAllIn(line).toList)
-      .map(dataExtraction)
+      .map(pointExtraction)
+
+  }
+
+  def loadRefData(spark: SparkSession, refGPSDataPath: String): RDD[(Point, String)] = {
+
+    val sc = spark.sparkContext
+
+    sc.textFile(refGPSDataPath)
+      .map(_.split(",").toList)
+      .map(referenceGPSDataExtraction)
+
+  }
+
+  def loadResultsData(spark: SparkSession, matchedGPSDataPath: String): RDD[(Point, String)] = {
+
+    val sc = spark.sparkContext
+
+    sc.textFile(matchedGPSDataPath)
+      .map(_.split(",").toList)
+      .map(matchedGPSDataExtraction)
 
   }
 
@@ -67,26 +87,32 @@ object Read {
 
   }
 
-  def dataExtraction(list: List[String]): Point = {
+
+  private def pointExtraction(list: List[String]): Point = {
     //We first put the 4th entry as it is the latitude and we want the LatLon array
     new Point(list(3).toDouble, list(2).toDouble, list(6).toDouble, list.head + " " + list(1))
 
   }
 
-  def matchedGPSDataExtraction(list: List[String]): (String, String) = {
+  private def matchedGPSDataExtraction(list: List[String]): (Point, String) = {
 
-    (list(1), list.head)
+    val point = new Point(list(2).toDouble, list(3).toDouble, list(1))
+
+    (point, list.head)
 
   }
 
-  def referenceGPSDataExtraction(list: List[String]): (String, String) = {
+  private def referenceGPSDataExtraction(list: List[String]): (Point, String) = {
 
     val pattern = "([^{}]+)".r
-    (list(1) + " " + list(2), pattern.findFirstIn(list(9)).get)
+    val id = list(1) + " " + list(2)
+    val point = new Point(list(4).toDouble, list(3).toDouble, id)
+
+    (point, pattern.findFirstIn(list(9)).get)
 
   }
 
-  def matchedGPSDataExtractionStats(list: List[String]): (Point, String, Double, Point) = {
+  private def matchedGPSDataExtractionStats(list: List[String]): (Point, String, Double, Point) = {
 
     val pattern1 = "[0-9]+".r
     val pattern2 = "[0-9]+.[0-9]+".r
